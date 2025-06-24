66594054473e66f2cb6f6d58edc8e96fdd2d5751
Nemuel Wainaina <nemuelwainaina@proton.me>
2025-06-24 15:22:51 +0300

    Update filenames & to include user-agent header in http requests

--- src/tpdne.nim
+++ src/tpdne.nim
@@ -1,10 +1,12 @@
-import httpclient, os
+import httpclient
 
 const imageUrl = "https://thispersondoesnotexist.com"
 
 proc fetchImage*(): seq[byte] =
   let client = newHttpClient()
-  # let image = getContent(client, imageUrl)
+  client.headers = newHttpHeaders({
+    "User-Agent": "tbdne/0.1.0 (Nim)" 
+  })
   let image = cast[seq[byte]](getContent(client, imageUrl))
   if image.len == 0:
     raise newException(IOError, "empty image received from api")
